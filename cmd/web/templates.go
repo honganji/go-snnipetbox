c0572fde2550438cf12314342b4a20685a29b89c
honganji <yujicavs@gmail.com>
2025-12-14 15:07:00 +0900

    add: reorganize imports and enhance template cache initialization

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,9 +1,45 @@
 package main
 
-import "github.com/honganji/go-snippetbox/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/honganji/go-snippetbox/internal/models"
+)
 
 // templateData holds data sent to HTML templates
 type templateData struct {
 	Snippet  models.Snippet
 	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	// initialize a new map to act as the cache
+	cache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+	// range through all the page templates
+	// store the resulting templates in the cache map
+	for _, page := range pages {
+		name := filepath.Base(page)
+		// parse the base template
+		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+		// parse any partials
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+		// parse the page template
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+		cache[name] = ts
+	}
+	return cache, nil
 }
