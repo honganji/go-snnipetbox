f402b070bb81fda4b2706fbee023984ba15871a9
honganji <yujicavs@gmail.com>
2025-12-20 16:04:26 +0900

    feat: implement user logout functionality with session management

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -210,7 +210,14 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Log out a user")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 type userLoginForm struct {
